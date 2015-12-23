7c0736b7266fcdb7659d3104b39022ddc51fc0c0
Eugene Domotenko <paintingduck92@gmail.com>
2015-12-23 04:41:58 +0200

    server

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -8,7 +8,19 @@ angular.module('confusionApp')
   $scope.filtText = '';
   $scope.showDetails = false;
 
-  $scope.dishes = menuFactory.getDishes();
+  $scope.showMenu = false;
+  $scope.message = "Loading ...";
+  $scope.dishes = {};
+  menuFactory.getDishes()
+    .then(
+      function(response) {
+        $scope.dishes = response.data;
+        $scope.showMenu = true;
+      },
+      function(response) {
+        $scope.message = "Error: " + response.status + " " + response.statusText;
+      }
+    );
 
 
   $scope.select = function(setTab) {
@@ -84,7 +96,19 @@ angular.module('confusionApp')
 
 .controller('DishDetailController', ['$scope', '$stateParams', 'menuFactory', function($scope, $stateParams, menuFactory) {
 
-  $scope.dish = menuFactory.getDish(parseInt($stateParams.id, 10));
+  $scope.dish = {};
+  $scope.showDish = false;
+  $scope.message = "Loading ...";
+  menuFactory.getDish(parseInt($stateParams.id, 10))
+    .then(
+      function(response) {
+        $scope.dish = response.data;
+        $scope.showDish = true;
+      },
+      function(response) {
+        $scope.message = "Error: " + response.status + " " + response.statusText;
+      }
+    );
 
 }])
 
@@ -118,8 +142,20 @@ angular.module('confusionApp')
 // implement the IndexController and About Controller here
 .controller('IndexController', ['$scope', 'corporateFactory', 'menuFactory', function($scope, corporateFactory, menuFactory) {
 
-  $scope.dish = menuFactory.getDish(0);
+  $scope.dish = {};
+  $scope.showDish = false;
+  $scope.message = "Loading ...";
 
+  menuFactory.getDish(0)
+    .then(
+      function(response) {
+        $scope.dish = response.data;
+        $scope.showDish = true;
+      },
+      function(response) {
+        $scope.message = "Error: " + response.status + " " + response.statusText;
+      }
+    );
   $scope.promotion = menuFactory.getPromotion(0);
 
   $scope.chef = corporateFactory.getLeader(3);
